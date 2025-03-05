e5d99b2678a95fff0a882c5195e21c87a4f0a942
johannpa <johann_paque@hotmail.fr>
2025-03-05 19:56:56 +0100

    Comment to know where the server run and the first route to test

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,10 @@ const port = 5000;
 
 const app = express();
 
+app.get("/post", (req, res) => {
+    res.json({ message: "Voici les données" });
+});
+
+
+// Run the server
 app.listen(port, () => console.log("Le server a démarré sur le port " + port));
